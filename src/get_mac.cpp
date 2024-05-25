f932cf04ce3e3b59358b1e9b174746a8a8f1e6b6
Spencer Owen <owenspencer@gmail.com>
2024-05-25 11:41:44 -0600

    Initial commit

---
+++ src/get_mac.cpp
@@ -0,0 +1,35 @@
+// #include <Arduino.h>
+// #include "WiFi.h"
+
+// int ledPin = 15;
+
+// void setup() {
+
+//   pinMode(ledPin, OUTPUT); // Set LED pin as output
+
+//     // Setup Serial Monitor
+//   Serial.begin(9600);
+ 
+//   // Put ESP32 into Station mode
+//   WiFi.mode(WIFI_MODE_STA);
+ 
+//   // Print MAC Address to Serial monitor
+//   Serial.print("MAC Address: ");
+//   Serial.println(WiFi.macAddress());
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   digitalWrite(ledPin, HIGH);   // Turn LED on
+//   delay(1000);                  // Wait for 1 second
+//   digitalWrite(ledPin, LOW);    // Turn LED off
+//   delay(1000);                  // Wait for 1 second
+//   // Serial.print("MAC Address: ");
+//   // Serial.println(WiFi.macAddress());
+// }
+
+// // // put function definitions here:
+// // int myFunction(int x, int y) {
+// //   return x + y;
+// // }
