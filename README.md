4a3a930e3a2fb653766552919d18e273d3093893
Spencer Owen <owenspencer@gmail.com>
2024-05-25 12:32:12 -0600

    hold button to turn on

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pool Remote
+
+Control a motor remotely using multiple ESP32 devices and the ESP-Now protocol. 
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,19 @@
-/*
-  ESP-NOW Multi Unit Demo
-  esp-now-multi.ino
-  Broadcasts control messages to all devices in network
-  Load script on multiple devices
-
-  DroneBot Workshop 2022
-  https://dronebotworkshop.com
-*/
-
-// Include Libraries
 #include <WiFi.h>
 #include <esp_now.h>
 
-// Define LED and pushbutton state booleans
 bool buttonDown = false;
 bool ledOn = false;
 
 // Define LED and pushbutton pins
 #define STATUS_LED 15
 #define STATUS_BUTTON 0
-
-
-void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength) {
 // Formats MAC Address
-{
   snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 }
 
-
-void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen) {
 // Called when data is received
-{
-  // Only allow a maximum of 250 characters in the message + a null terminating byte
   char buffer[ESP_NOW_MAX_DATA_LEN + 1];
   int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
   strncpy(buffer, (const char *)data, msgLen);
@@ -47,21 +29,16 @@ void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen)
   Serial.printf("Received message from: %s - %s\n", macStr, buffer);
 
   // Check switch status
-  if (strcmp("on", buffer) == 0)
-  {
+  if (strcmp("on", buffer) == 0) {
     ledOn = true;
-  }
-  else
-  {
+  } else {
     ledOn = false;
   }
   digitalWrite(STATUS_LED, ledOn);
 }
 
-
-void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
-// Called when data is sent
-{
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status) {
+  // Called when data is sent
   char macStr[18];
   formatMacAddress(macAddr, macStr, 18);
   Serial.print("Last Packet Sent to: ");
@@ -70,54 +47,36 @@ void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status)
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void broadcast(const String &message)
-// Emulates a broadcast
-{
-  // Broadcast a message to every device in range
+void broadcast(const String &message) {
+  // Emulates a broadcast
   uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
   esp_now_peer_info_t peerInfo = {};
   memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
-  if (!esp_now_is_peer_exist(broadcastAddress))
-  {
+  if (!esp_now_is_peer_exist(broadcastAddress)) {
     esp_now_add_peer(&peerInfo);
   }
   // Send message
   esp_err_t result = esp_now_send(broadcastAddress, (const uint8_t *)message.c_str(), message.length());
 
   // Print results to serial monitor
-  if (result == ESP_OK)
-  {
+  if (result == ESP_OK) {
     Serial.println("Broadcast message success");
-  }
-  else if (result == ESP_ERR_ESPNOW_NOT_INIT)
-  {
+  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
     Serial.println("ESP-NOW not Init.");
-  }
-  else if (result == ESP_ERR_ESPNOW_ARG)
-  {
+  } else if (result == ESP_ERR_ESPNOW_ARG) {
     Serial.println("Invalid Argument");
-  }
-  else if (result == ESP_ERR_ESPNOW_INTERNAL)
-  {
+  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
     Serial.println("Internal Error");
-  }
-  else if (result == ESP_ERR_ESPNOW_NO_MEM)
-  {
+  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
     Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-  }
-  else if (result == ESP_ERR_ESPNOW_NOT_FOUND)
-  {
+  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
     Serial.println("Peer not found.");
-  }
-  else
-  {
+  } else {
     Serial.println("Unknown error");
   }
 }
 
-void setup()
-{
-
+void setup() {
   // Set up Serial Monitor
   Serial.begin(115200);
   delay(1000);
@@ -134,14 +93,11 @@ void setup()
   WiFi.disconnect();
 
   // Initialize ESP-NOW
-  if (esp_now_init() == ESP_OK)
-  {
+  if (esp_now_init() == ESP_OK) {
     Serial.println("ESP-NOW Init Success");
     esp_now_register_recv_cb(receiveCallback);
     esp_now_register_send_cb(sentCallback);
-  }
-  else
-  {
+  } else {
     Serial.println("ESP-NOW Init Failed");
     delay(3000);
     ESP.restart();
@@ -154,36 +110,22 @@ void setup()
   pinMode(STATUS_LED, OUTPUT);
 }
 
-void loop()
-{
-  if (digitalRead(STATUS_BUTTON))
-  {
-    // Detect the transition from low to high
-    if (!buttonDown)
-    {
+void loop() {
+  if (digitalRead(STATUS_BUTTON) == LOW) {
+    if (!buttonDown) {
       buttonDown = true;
-      
-      // Toggle the LED state
-      ledOn = !ledOn;
+      ledOn = true;
       digitalWrite(STATUS_LED, ledOn);
-      
-      // Send a message to all devices
-      if (ledOn)
-      {
-        broadcast("on");
-      }
-      else
-      {
-        broadcast("off");
-      }
+      broadcast("on");
     }
-    
-    // Delay to avoid bouncing
-    delay(500);
+  } else {
+    if (buttonDown) {
+      buttonDown = false;
+      ledOn = false;
+      digitalWrite(STATUS_LED, ledOn);
+      broadcast("off");
+    }
   }
-  else
-  {
-    // Reset the button state
-    buttonDown = false;
-  }
+
+  delay(100);  // Small delay to debounce the button
 }
